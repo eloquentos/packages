1e601ce85c7f06c096d106a90cffa29eedcf30e8
Chris Harvey <chris@chrisnharvey.com>
2016-01-10 20:11:23 +0000

    Don't confirm when installing deps

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ mkdir /tmp/packages
 for i in * ; do
     if [ -d "$i" ]; then
         cd $i
-        makepkg -C -f -s -r --sign
+        makepkg -C -f -s -r --sign --noconfirm
         mv *.pkg.* /tmp/packages
         cd ..
     fi
