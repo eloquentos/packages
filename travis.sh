4721668e573f580f780f7b2037219387350596f4
Chris Harvey <chris@chrisnharvey.com>
2016-07-31 23:31:49 +0100

    Install git

--- travis.sh
+++ travis.sh
@@ -1,4 +1,4 @@
-pacman -S sudo base-devel --noconfirm
+pacman -S sudo base-devel git --noconfirm
 echo "%wheel ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/wheel
 useradd eloquentos
 usermod -aG wheel eloquentos
