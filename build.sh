8408b1e91c8a98568f5e5deaf7472913c0b2d1d7
Chris Harvey <chris@chrisnharvey.com>
2016-08-01 12:45:05 +0100

    Use our key to sign our packages

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ mkdir /tmp/packages
 for i in * ; do
     if [ -d "$i" ]; then
         cd $i
-        makepkg -C -f -s -r --sign --key="/mnt/sign-key" --noconfirm
+        makepkg -C -f -s -r --sign --key="support@nimbusoft.co.uk" --noconfirm
         mv *.pkg.* /tmp/packages
         cd ..
     fi
