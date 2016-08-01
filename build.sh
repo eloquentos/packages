edb6b38d863b55ff02505206839bf35accf837d6
Chris Harvey <chris@chrisnharvey.com>
2016-08-01 20:56:41 +0100

    Sign our packages

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ mkdir /tmp/packages
 for i in * ; do
     if [ -d "$i" ]; then
         cd $i
-        makepkg -C -f -s -r --nosign --noconfirm
+        makepkg -C -f -s -r --sign --key eloquentos@nimbusoft.co.uk --noconfirm
         mv *.pkg.* /tmp/packages
         cd ..
     fi
